99b10310b450ff498642ec858dfe2b96503877ab
Carlos R. Rendon <carlangass002@hotmail.com>
2021-02-04 20:11:04 -0600

    Agrega la funcionalidad de añadir task

--- js/code.js
+++ js/code.js
@@ -17,22 +17,28 @@ Todos los elementos visibles deben ser creados usando funciones de JavaScript, e
 
 
 //Selector de contenedor padre, el unico elemento HTML div#app
-parentContainer = document.getElementById('app');
+let parentContainer = document.getElementById('app');
 //parentContainer.style.border= '3px solid red';
 
 //Se crea el elemento del titulo H1 y se agrega al contenedor padre
-title = document.createElement('h1');
+let title = document.createElement('h1');
 title.textContent = 'To Do App';
 parentContainer.appendChild(title);
 
 //Se crea el contenedor para el input y el boton de add
-textBoxContainer = document.createElement('div');
-textBoxContainer.id = 'textbox-container';
+let textBoxContainer = document.createElement('div');
+textBoxContainer.className += 'textbox-container';
 
 //Creacion del input text y del boton add
-textBox = document.createElement('input');
-addButton = document.createElement('button');
+let textBox = document.createElement('input')
+textBox.setAttribute('placeholder','New Task...') ;
+let addButton = document.createElement('button');
 addButton.textContent = 'Add';
+
+//Se agregan los eventos al boton add (addButton)
+addButton.addEventListener('click',() => {
+    TaskCreator();
+});
 
 
 //Se agregan los elementos creados al contenedor
@@ -41,6 +47,41 @@ textBoxContainer.appendChild(addButton);
 
 //Se agregan los nuevos elementos al contendor padre
 parentContainer.appendChild(textBoxContainer);
+
+//Se crea el contenedor para las task
+let taskContainer = document.createElement('div');
+taskContainer.className += 'task-container';
+parentContainer.appendChild(taskContainer);
+
+
+//Se define una funcion para crear las tareas y agregarlas al contenedor de tareas (taskContainer)
+function TaskCreator(){
+
+    let taskDiv = document.createElement('div');
+
+    let elementGroup = {
+        checkboxBtn : document.createElement('input'),
+        label : document.createElement('label').appendChild(document.createTextNode(textBox.value)),
+        button : document.createElement('button')
+    };
+
+   elementGroup.checkboxBtn.setAttribute('type','checkbox');
+   elementGroup.button.textContent = 'Delete';
+
+   Object.values(elementGroup).forEach(prop => {
+       taskDiv.appendChild(prop);
+   });
+
+   taskContainer.appendChild(taskDiv);
+   textBox.value = '';
+   };
+
+  
+
+   
+
+  
+   
 
 
 
